03ac796f23aa68c0ab78c432a7316444e0605fe5
Brian Shirai <brixen@gmail.com>
2014-03-14 09:09:05 -0500

    Fixed compiled file specs.

--- spec/compiled_file/marshal/unmarshal_spec.rb
+++ spec/compiled_file/marshal/unmarshal_spec.rb
@@ -1,79 +1,57 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe "Rubinius::CompiledFile::Marshal#unmarshal" do
+describe "Rubinius::ToolSets::Runtime::CompiledFile::Marshal#unmarshal" do
+  before :each do
+    @marshal = Rubinius::ToolSets::Runtime::CompiledFile::Marshal.new
+  end
+
   it "unmarshals true" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("t\n").should == true
+    @marshal.unmarshal("t\n").should == true
   end
 
   it "unmarshals false" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("f\n").should == false
+    @marshal.unmarshal("f\n").should == false
   end
 
   it "unmarshals nil" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("n\n").should == nil
+    @marshal.unmarshal("n\n").should == nil
   end
 
   it "unmarshals an Integer" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("I\nCAFEF00D").should == 0xCAFEF00D
+    @marshal.unmarshal("I\nCAFEF00D").should == 0xCAFEF00D
   end
 
   it "unmarshals a Float" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("d\n +0.785000000000000031086244689504383131861686706542968750     2\n").should == 3.14
+    @marshal.unmarshal("d\n +0.785000000000000031086244689504383131861686706542968750     2\n").should == 3.14
   end
 
   it "unmarshals +infinity" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("d\nInfinity\n").should be_positive_infinity
+    @marshal.unmarshal("d\nInfinity\n").should be_positive_infinity
   end
 
   it "unmarshals -infinity" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("d\n-Infinity\n").should be_negative_infinity
+    @marshal.unmarshal("d\n-Infinity\n").should be_negative_infinity
   end
 
   it "unmarshals NaN" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("d\nNaN\n").should be_nan
+    @marshal.unmarshal("d\nNaN\n").should be_nan
   end
 
   it "unmarshals a Symbol" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("x\nE\n8\nUS-ASCII\n12\nHello world!\n").should == :"Hello world!"
+    @marshal.unmarshal("x\nE\n8\nUS-ASCII\n12\nHello world!\n").should == :"Hello world!"
   end
 
   it "unmarshals a constant" do
-    marshal = Rubinius::CompiledFile::Marshal.new
-    marshal.unmarshal("c\n22\nRubinius::CompiledFile\n").should == Rubinius::CompiledFile
+    @marshal.unmarshal("c\n18\nRubinius::ToolSets\n").should == Rubinius::ToolSets
   end
 
-  ruby_version_is ""..."1.9" do
-    it "unmarshals a String" do
-      marshal = Rubinius::CompiledFile::Marshal.new
-      marshal.unmarshal("s\nE\n0\n\n12\nHello world!\n").should == "Hello world!"
-    end
-
-    it "returns nil when unmarshalling an encoding" do
-      marshal = Rubinius::CompiledFile::Marshal.new
-      marshal.unmarshal("E\n5\nUTF-8\n13\n").should be_nil
-    end
+  it "unmarshals a String" do
+    str = @marshal.unmarshal("s\nE\n5\nUTF-8\n12\nHello world!\n")
+    str.should == "Hello world!"
+    str.encoding.should == Encoding::UTF_8
   end
 
-  ruby_version_is "1.9" do
-    it "unmarshals a String" do
-      marshal = Rubinius::CompiledFile::Marshal.new
-      str = marshal.unmarshal("s\nE\n5\nUTF-8\n12\nHello world!\n")
-      str.should == "Hello world!"
-      str.encoding.should == Encoding::UTF_8
-    end
-
-    it "unmarshals an encoding" do
-      marshal = Rubinius::CompiledFile::Marshal.new
-      marshal.unmarshal("E\n5\nUTF-8\n13\n").should == Encoding::UTF_8
-    end
+  it "unmarshals an encoding" do
+    @marshal.unmarshal("E\n5\nUTF-8\n13\n").should == Encoding::UTF_8
   end
 end
